b52be6ab68386783495eb97bd6349fdb0d6056d8
Agharese Blessing <blessing4coding@gmail.com>
2025-06-29 02:09:42 +0100

    used bootstrap

---
+++ index.js
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>ANTO – Faith. Creativity. Community.</title>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <img class="logo desktop-logo" src="media/anto-logo.png" alt="ANTO Logo Desktop">
+    <img class="logo mobile-logo" src="media/anto-logo2.png" alt="ANTO Logo Mobile">
+
+  <header>
+    <h1>Next Gen Afri
+    <br> Christian Social App </h1>
+    <p>Faith. Creativity. Community.</p>
+  </header>
+
+  <section class="mission">
+    <div class="mission-vission">
+    <h2>Our Mission</h2>
+    <p>ANTO exists to create a safe, faith-rooted digital space where people can express their walk with God, connect with others on the same journey, and grow in hope, creativity, and purpose without fear of judgment or toxicity.</p>
+    <h2>Our Vision</h2>
+    <p>To become the go-to global platform where faith, creativity, and community meet, offering light in the noise of today’s online world.</p>
+   </div>
+</section>
+
+  <section class="section">
+    <h2>Why ANTO?</h2>
+    <p>A safe, faith-rooted space where believers can post, pray, praise, and grow — without judgment or digital noise.</p>
+    <div class="features">
+      <div class="feature-box">
+        <h3>✍️ Express</h3>
+        <p>Share devotionals, testimonies, and art from your heart.</p>
+      </div>
+      <div class="feature-box">
+        <h3>🙏 Pray</h3>
+        <p>Join a global prayer wall and uplift others in faith.</p>
+      </div>
+      <div class="feature-box">
+        <h3>🎵 Create</h3>
+        <p>Upload and share gospel music, spoken word, or podcasts.</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="cta">
+    <h2>Join the Movement</h2>
+    <p>Be the first to know when ANTO launches</p>
+    <form action="https://docs.google.com/forms/d/e/1FAIpQLSc6lzeq--TSXqPYacSqAy6wSpHGQ1zJP_IHj06dIkrurSX8jA/formResponse" method="POST" target="_blank">
+        <input name="entry.1187965032" type="email" placeholder="Your email" required />
+        <button type="submit">Notify Me</button>
+      </form>
+      
+  </section>
+
+  <footer>
+    <p>&copy; 2025 ANTO. Built with purpose by Agharese Blessing.</p>
+  </footer>
+</body>
+</html>
